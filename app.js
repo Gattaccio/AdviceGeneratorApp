0df0bbb6d80f6f620d339cc5f02042a2ab27fdd3
Gattaccio <87563233+Gattaccio@users.noreply.github.com>
2022-06-03 17:23:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ async function getQuote () {
  const response = await fetch ('https://api.adviceslip.com/advice'); 
  const data = await response.json(); 
  quote.innerHTML=data.slip.advice;
- console.log(data.slip.advice);
 }
 
 dice.addEventListener('click', ()=> {
