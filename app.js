7ebd76b0bbfdbbfb12345dfa323b69e5ba93b6a0
Gattaccio <87563233+Gattaccio@users.noreply.github.com>
2022-06-03 17:24:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-let quote = document.querySelector('.advice');
+var quote = document.querySelector('.advice');
 let dice = document.querySelector('#dice');
+var nadvice = document.querySelector('#nadvice');
 
-getQuote();
+var idrand = Math.floor(Math.random() * 220) + 1;
+getQuote(idrand);
 
-async function getQuote () {
- const response = await fetch ('https://api.adviceslip.com/advice'); 
+async function getQuote (idrand) {
+ var prova = 'https://api.adviceslip.com/advice/'+idrand;
+ console.log(prova);
+ const response = await fetch (prova); 
  const data = await response.json(); 
  quote.innerHTML=data.slip.advice;
+ nadvice.innerHTML=data.slip.id;
 }
 
 dice.addEventListener('click', ()=> {
-    location.reload();
+    var idrand = Math.floor(Math.random() * 220) + 1;
+    getQuote(idrand);
 })
-
-
-
