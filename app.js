bcd0d5f66b0495b4fd00f38c1f3039a71c48fb6c
Gattaccio <87563233+Gattaccio@users.noreply.github.com>
2022-06-03 17:28:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ getQuote(idrand);
 async function getQuote (idrand) {
  var random_quote = 'https://api.adviceslip.com/advice/'+idrand;
  console.log(random_quote);
- const response = await fetch (prova); 
+ const response = await fetch (random_quote); 
  const data = await response.json(); 
  quote.innerHTML=data.slip.advice;
  nadvice.innerHTML=data.slip.id;
